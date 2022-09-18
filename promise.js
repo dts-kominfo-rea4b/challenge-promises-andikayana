1f6fecbcf3f4bfb1ab8e54051e447a3f2d6bd925
andikayana <andikayana99@gmail.com>
2022-09-18 16:59:22 +0800

    add promise

--- promise.js
+++ promise.js
@@ -1,8 +1,24 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const {
+    promiseTheaterIXX,
+    promiseTheaterVGC
+} = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+    try {
+        const promiseOne = await promiseTheaterIXX();
+        const promiseTwo = await promiseTheaterVGC();
+
+        const output  = [...promiseOne, ...promiseTwo]
+            .filter((value) => value.hasil === emosi)
+            .length;
+        return output;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
